c17df5bd253e2a55bd8464b572dba2296a907448
filip106 <djordjevic.filip994@gmail.com>
2015-05-11 18:49:41 +0200

    napravljana metoda ucitaj iz fajla

--- Test Maker/src/logicka_kontrola/TestMaker.java
+++ Test Maker/src/logicka_kontrola/TestMaker.java
@@ -1,5 +1,11 @@
 package logicka_kontrola;
 
+import java.util.LinkedList;
+
 public class TestMaker {
+
+	LinkedList<Pitanja> listaPitanja = new LinkedList<Pitanja>();
+	
+	public void ucitajIzFajla(){}
 
 }
---
+++ Test Maker/src/sistemskeoperacije/SOUcitajIzFajla.java
@@ -0,0 +1,5 @@
+package sistemskeoperacije;
+
+public class SOUcitajIzFajla {
+
+}
