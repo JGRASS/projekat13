c3891eb3df76d374a49c461dd31e0f4eb21c1d95
filip106 <djordjevic.filip994@gmail.com>
2015-05-11 20:28:58 +0200

    Dodata metoda napravi test

--- Test Maker/src/sistemskeoperacije/SOUcitajIzFajla.java
+++ Test Maker/src/sistemskeoperacije/SOUcitajIzFajla.java
@@ -1,5 +1,31 @@
 package sistemskeoperacije;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+
+import logicka_kontrola.Pitanja;
+
 public class SOUcitajIzFajla {
 
+	public static void ucitajIzFajla(String naziv, LinkedList<Pitanja> listaPitanja){
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(naziv));
+			listaPitanja.clear();
+			
+			while(true){
+				String pom = in.readLine();
+				if(pom==null) break;
+				
+				Pitanja pitanje = new Pitanja();
+				pitanje.setPitanje(pom);
+				
+				listaPitanja.add(pitanje);
+			}
+			
+			in.close();
+		} catch (Exception e) {
+		}
+	}
 }
