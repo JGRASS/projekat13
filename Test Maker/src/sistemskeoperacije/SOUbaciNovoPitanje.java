351750e71ef5303a28d0e46a2aac12a33435eb6d
deffaccto <uros.spasojevic.94@gmail.com>
2015-05-14 22:49:10 +0200

    prepravljena je metoda SOUbaciNovoPitanje

--- Test Maker/src/sistemskeoperacije/SOUbaciNovoPitanje.java
+++ Test Maker/src/sistemskeoperacije/SOUbaciNovoPitanje.java
@@ -1,9 +1,12 @@
 package sistemskeoperacije;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.LinkedList;
 
 import javax.swing.JTextField;
 
@@ -13,10 +16,24 @@ public class SOUbaciNovoPitanje {
 	public static void upisiPitanje(String naziv,String pitanje){
 		
 		try {
-			PrintWriter out=new PrintWriter(new BufferedWriter(new FileWriter(naziv+".txt")));
+			BufferedReader in = new BufferedReader(new FileReader(naziv+",txt"));
+			LinkedList<String> listaPitanja=new LinkedList<String>();
+			while(true){
+				String pom=in.readLine();
+				if(pom==null) break;
+				
+				listaPitanja.add(pom);
+			}
+			listaPitanja.add(pitanje);
 			
-			out.append(pitanje);
+			PrintWriter out=new PrintWriter(new BufferedWriter(new FileWriter(naziv+".txt")));
+			for (int i = 0; i < listaPitanja.size(); i++) {
+				out.append(listaPitanja.get(i).toString());
+			}
 			out.close();
+			in.close();
+					
+						
 		} 
 		
 		catch (IOException e) {
